e3e99cc545aefdd2456a3822c500b7f1a7f175e6
hammadulhaq01 <hammad.ul.haq@thesmallsquare.com>
2025-07-21 12:02:48 +0500

    Added a navigator that navigates to login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
 import Currencies from './pages/Currencies'
@@ -8,6 +8,7 @@ function App() {
 
   return (
   <Routes>
+    <Route path="/" element={<Navigate to="/login" replace />} />
     <Route path='/login' element={<Login/>} />
     <Route path='/currencies' element={<AppLayout children={<Currencies/>} />} />
 
